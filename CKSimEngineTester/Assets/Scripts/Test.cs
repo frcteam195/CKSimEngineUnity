f9d9031d96eed71255195335186697b49f3dcab1
Thomas Lourenco <TSLourenco@WPI.edu>
2020-10-04 11:40:52 -0400

    Updated the committed files for the tester to match the updated .gitignore file.

---
+++ CKSimEngineTester/Assets/Scripts/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public string LatestTopic, LatestData;
+
+    public Text TextObject;
+
+    [DllImport ("CKSimEngineUnityPlugin")]
+
+    private static extern float FooPluginFunction();
+
+    void Start()
+    {
+        UnityEngine.Debug.Log("Starting test...");
+
+        TextObject = GameObject.Find("Text").GetComponent<Text>();
+
+        UnityEngine.Debug.Log("Foo Function from C++ DLL: " + FooPluginFunction().ToString());
+    }
+
+    void Update()
+    {
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Application ending after " + Time.time + " seconds.");
+    }
+}
