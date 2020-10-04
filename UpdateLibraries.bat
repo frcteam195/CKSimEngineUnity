025355b90bca799628be117f233a8b009807f524
Thomas Lourenco <TSLourenco@WPI.edu>
2020-10-04 12:30:43 -0400

    Added line for the CKlibzmq DLL to the UpdateLibraries.bat script. Added CKlibzmq as a submodule.

--- UpdateLibraries.bat
+++ UpdateLibraries.bat
@@ -1,5 +1,8 @@
 :: Copy the C++ DLL.
-copy "CKSimEnginePlugin\CKSimEngineUnityPlugin\CKSimEngineUnityPlugin\lib\Debug\x64\CKSimEngineUnityPlugin.dll" "CKSimEngineTester/Assets/Plugins/"
+copy "CKSimEnginePlugin\CKSimEngineUnityPlugin\lib\Debug\x64\CKSimEngineUnityPlugin.dll" "CKSimEngineTester/Assets/Plugins/"
+
+:: Copy the DLL dependencies.
+copy "CKSimEnginePlugin\CKlibzmq\bin\libzmq-v142-mt-4_3_3.dll" "CKSimEngineTester/Assets/Plugins/"
 
 :: Pause to debug.
 pause
